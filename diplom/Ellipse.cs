940900c362ecbd65ae7419f08476dfedf510e8b4
qwertyV112 <strelocpm@gmail.com>
2015-05-09 00:21:52 +0400

    Ellipse

---
+++ diplom/Ellipse.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace diplom
+{
+    class Ellipse
+    {
+        public double x;
+        public double y;
+        public double Radiys;
+        private Graphics Object;
+        public Ellipse(Graphics image,double middleX, double middleY)
+        {
+            
+        }
+        public void Clear()
+        {
+
+        }
+        public void Render()
+        {
+            
+        }
+    }
+}
--- diplom/Form1.cs
+++ diplom/Form1.cs
@@ -12,9 +12,31 @@ namespace diplom
 {
     public partial class Form1 : Form
     {
+        public Graphics image;
+        public Pen pen;
         public Form1()
         {
             InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+
+            
+            
+          //  image.DrawEllipse(p, 10, 10, 40, 40); 
+        }
+        private void FormsSetting(){
+            image = pictureBox1.CreateGraphics();
+            Pen p = new Pen(Color.Red);
+        }
+        private void Ellipse()
+        {
+
+        }
+        private void pictureBox1_Click(object sender, EventArgs e)
+        {
+
         }
     }
 }
